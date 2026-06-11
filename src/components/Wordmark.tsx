a33dce0b43e10d2312847b0e512a253fbd9d1011
Eduardo Valle <eduardovalle.10@gmail.com>
2026-06-11 10:21:56 -0400

    update logo i dot to gold coin style

--- src/components/Wordmark.tsx
+++ src/components/Wordmark.tsx
@@ -19,11 +19,21 @@ export function Wordmark({ className }: WordmarkProps) {
       aria-label="InvestiPlay"
     >
       <span aria-hidden="true">Invest</span>
-      {/* lowercase dotless "i" (U+0131) with a dollar-sign-style dot */}
+      {/* lowercase dotless "i" (U+0131) whose dot is a gold $ coin */}
       <span aria-hidden="true" className="relative inline-block">
         ı
-        <span className="absolute left-1/2 -translate-x-1/2 -top-[0.12em] text-[0.55em] leading-none">
-          $
+        <span
+          className="absolute left-1/2 -translate-x-1/2 -top-[0.30em] box-border inline-flex items-center justify-center rounded-full"
+          style={{
+            width: "0.5em",
+            height: "0.5em",
+            backgroundColor: "#F5A623",
+            border: "0.055em solid #B97509",
+          }}
+        >
+          <span style={{ color: "#6B4A0F", fontSize: "0.34em", fontWeight: 800, lineHeight: 1 }}>
+            $
+          </span>
         </span>
       </span>
       <span aria-hidden="true">Play</span>
